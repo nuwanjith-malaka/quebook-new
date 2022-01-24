4f7be4f43a5638110db54dc88cd8ded40a439637
nuwanjith-malaka <nuwanjithm@gmail.com>
2022-01-24 19:29:56 +0530

    Revert "Revert "created .env file for secret key""
    This reverts commit 6691f293abeec8040528bfd721cb98dd6f3de7b6.

--- qbooknew/settings.py
+++ qbooknew/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('DJANGO_SECRET_KEY')
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
