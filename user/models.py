28a27d261dda5ea7df56ff0811dbd233ddbac584
nuwanjith-malaka <nuwanjithm@gmail.com>
2022-01-24 19:45:32 +0530

    Revert "changed asker profile model"
    This reverts commit d2b9cf6de39d8b0934053417fc716eb7236605c7.

--- user/models.py
+++ user/models.py
@@ -1,20 +1,15 @@
-from random import choices
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 # Create your models here.
-GRADE = [
-    ('10','10'),
-    ('11','11')
-]
 class AskerProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(default="john", max_length=20)
-    last_name = models.CharField(default="doe", max_length=20)
+    first_name = models.CharField(max_length=20, null=True, blank=True)
+    last_name = models.CharField(max_length=20, null=True, blank=True)
     photo = models.ImageField(null=True, blank=True)
-    school = models.CharField(default='sample shool', max_length=50)
-    grade = models.CharField(choices=GRADE, default='10', max_length=2)
-    city = models.CharField(default='sample city', max_length=20)
+    age = models.IntegerField(null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    website = models.URLField(null=True, blank=True)
 
     def get_absolute_url(self):
         return reverse('asker', kwargs={'pk': self.user.pk})
