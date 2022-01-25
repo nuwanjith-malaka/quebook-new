007da4037d76ecb2aa99a1c86a829066093ac054
nuwanjith-malaka <nuwanjithm@gmail.com>
2022-01-25 11:23:50 +0530

    added fields to askerprofile model

--- user/models.py
+++ user/models.py
@@ -2,12 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 # Create your models here.
+GRADE = [
+    ('10','10'),
+    ('11','11')
+]
 class AskerProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=20, null=True, blank=True)
     last_name = models.CharField(max_length=20, null=True, blank=True)
     photo = models.ImageField(null=True, blank=True)
-    
+    school = models.CharField(max_length=20, null=True, blank=True)
+    grade = models.CharField(choices=GRADE, max_length=20, null=True, blank=True)
+    city = models.CharField(max_length=20, null=True, blank=True)
 
     def get_absolute_url(self):
         return reverse('asker', kwargs={'pk': self.user.pk})
