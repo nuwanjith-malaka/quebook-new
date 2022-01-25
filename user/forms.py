1a4a1daa826b1d6ecbed66943715ff1dd475e50e
nuwanjith-malaka <nuwanjithm@gmail.com>
2022-01-25 11:35:40 +0530

    changed asker profile template and form

--- user/forms.py
+++ user/forms.py
@@ -9,7 +9,7 @@ class AskerProfileForm(forms.ModelForm):
             'first_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}),
             'last_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}),
             'photo': forms.FileInput(attrs={'class': 'form-control', 'placeholder': 'Photo'}),
-            'age': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Age'}),
-            'address': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Address'}),
-            'Website': forms.URLInput(attrs={'class': 'form-control', 'placeholder': 'Website'}),
+            'school': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'School'}),
+            'grade': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Grade'}),
+            'city': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'City'}),
         }
